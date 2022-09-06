c2bf40ab7a5be3a321d04eac4b7e0a8166db9fb5
Dave C. Bulaklak <dave.c.bulaklak@prulifeuk.com.ph>
2022-09-06 23:18:13 +0800

    Fix error in compatibility in react

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import * as ReactDOMClient from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOMClient.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.Fragment>
     <App />
-  </React.Fragment>
+  </React.Fragment>,
+  document.getElementById("root")
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
